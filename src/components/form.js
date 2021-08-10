22d6f76408b41aad6646b3d933abb545f4c7581e
e0marina <emilymarinaro@gmail.com>
2021-08-10 09:56:52 -0700

    getting somewhere with validation

--- src/components/form.js
+++ src/components/form.js
@@ -1,162 +1,123 @@
-import React, { useState } from 'react';
-import { Form, Button, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+// import { Form, Button, Col } from 'react-bootstrap';
+//import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const FormWrapper = () => {
-  const formFieldArr = [
-    {
-      id: 'formBasicFirstName',
-      label: 'First Name *',
-      placeholder: 'First Name',
-      name: 'firstname',
-    },
-    {
-      id: 'formBasicLastName',
-      label: 'Last Name *',
-      placeholder: 'Last Name',
-      name: 'lastname',
-    },
-    {
-      id: 'formBasicEmail',
-      label: 'Email',
-      placeholder: 'Email',
-      name: 'email',
-    },
-    {
-      id: 'formBasicState',
-      label: 'State',
-      placeholder: 'State',
-      name: 'state',
-    },
-    {
-      id: 'formBasicParty',
-      label: 'Party',
-      placeholder: 'Party',
-      name: 'party',
-    },
-    {
-      id: 'formBasicTwitter',
-      label: 'Twitter',
-      placeholder: 'Twitter',
-      name: 'twitter',
-    },
-    {
-      id: 'formBasicFacebook',
-      label: 'Facebook',
-      placeholder: 'Facebook',
-      name: 'facebook',
-    },
-    {
-      id: 'formBasicInstagram',
-      label: 'Instagram',
-      placeholder: 'Instagram',
-      name: 'instagram',
-    },
-    {
-      id: 'formBasicImageUrl',
-      label: 'Image Url',
-      placeholder: 'Image Url',
-      name: 'imageurl',
-    },
-  ];
-
-  const initialInputState = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    state: '',
-    party: '',
-    twitter: '',
-    facebook: '',
-    instagram: '',
-    imageurl: '',
-    textarea: '',
-  };
-  const [eachEntry, setEachEntry] = useState(initialInputState);
-
-  const {
-    firstname,
-    lastname,
-    email,
-    state,
-    party,
-    twitter,
-    facebook,
-    instagram,
-    imageurl,
-    textarea,
-  } = eachEntry;
-
-  const handleInputChange = (e) => {
-    setEachEntry({ ...eachEntry, [e.target.name]: e.target.value });
-  };
-
-  const [checked, setChecked] = useState(false);
-  const handleCheckClick = () => setChecked(!checked);
-
-  const handleSubmit = (e) => {
-    localStorage.setItem('candidate', JSON.stringify(eachEntry));
-    window.scrollTo(0, 0);
-  };
-
   return (
     <>
-      <Col></Col>
-      <Col>
-        <Form className='form-body'>
-          {formFieldArr.map((field, idx) => (
-            <Form.Group className='mb-3' controlId={field.id} key={idx}>
-              <Form.Label className='form-label'>{field.label}</Form.Label>
-              <Form.Control
-                size='sm'
-                type='text'
-                name={field.name}
-                placeholder={field.placeholder}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          ))}
-          <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
-            <Form.Label>Additional Information</Form.Label>
-            <Form.Control
-              as='textarea'
-              rows={5}
-              name='textarea'
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-            <Form.Check
-              className='check'
-              name='checkbox'
-              type='checkbox'
-              onClick={handleCheckClick}
-              label='By checking this box, you acknowledge you are at least 18 years of age.'
-            />
-          </Form.Group>
-        </Form>
-        <div className='btn-group'>
-          <Link to='/success'>
-            <Button
-              className='btn'
-              variant='success'
-              type='submit'
-              onClick={handleSubmit}
-            >
-              SUBMIT
-            </Button>
-          </Link>
-          <Link to='/finish'>
-            <Button className='btn' id='white-btn'>
-              FINISHED
-            </Button>
-          </Link>
-          <Button className='btn' id='orange-btn'>
-            LIST ALL
-          </Button>
-        </div>
-      </Col>
-      <Col></Col>
+      {/* <Col></Col>
+      <Col> */}
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          state: '',
+          party: '',
+          twitter: '',
+          facebook: '',
+          instagram: '',
+          imageurl: '',
+          content: '',
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.firstName) {
+            errors.first = 'Required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 1000);
+          // if (values.firstName && values.lastName) {
+          //   console.log(values);
+          // } else {
+          //   console.log('required');
+          // }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className='form-group'>
+              <label htmlFor='firstName'>First Name*</label>
+              <Field name='firstName' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='lastName'>Last Name*</label>
+              <Field name='lastName' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <Field name='email' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='state'>State</label>
+              <Field name='state' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='party'>Party</label>
+              <Field name='party' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='twitter'>Twitter</label>
+              <Field name='twitter' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='facebook'>Facebook</label>
+              <Field name='facebook' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='instagram'>Instagram</label>
+              <Field name='instagram' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='imageurl'>Image URL</label>
+              <Field name='imageurl' className='form-control' type='text' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='content'>Additional Information</label>
+              <Field name='content' className='form-control' as='textarea' />
+            </div>
+            <div className='form-group'>
+              <button
+                type='submit'
+                className='btn btn-primary'
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? 'Please wait...' : 'Submit'}
+              </button>
+            </div>
+            {/* < div className='btn-group'>
+                <Link to='/success'>
+                  <Button
+                    className='btn'
+                    variant='success'
+                    type='submit'
+                    onClick={handleSubmit}
+                  >
+                    SUBMIT
+                  </Button>
+                </Link>
+
+                <Link to='/finish'>
+                  <Button className='btn' id='white-btn'>
+                    FINISHED
+                  </Button>
+                </Link>
+                <Button className='btn' id='orange-btn'>
+                  LIST ALL
+                </Button>
+              </div> */}
+          </Form>
+        )}
+      </Formik>
+      {/* </Col>
+      <Col></Col> */}
     </>
   );
 };
